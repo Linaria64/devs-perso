c4e74d8962e2bb8b46768441e8c01f848c1ad56a
Linaria64 <dorianpro77@proton.me>
2024-05-06 22:57:48 +0200

    Mise en place Du JSX

--- CV-Web/cv_web/src/app/page.tsx
+++ CV-Web/cv_web/src/app/page.tsx
@@ -1,113 +1,244 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+/**
+ * Menu de navigation
+ * @returns NavJSX
+ */
+function Nav() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <header className="  bg-gray-500">
+    <nav>
+      <ol className="flex gap-2">
+        <li><a href="#about">A Propos</a></li>
+        <li><a href="#skills">Compétence</a></li>
+        <li><a href="#">Parcours</a></li>
+        <li><a href="#">Portofolio</a></li>
+        <li><a href="#">Contact</a></li>
+      </ol>
+    </nav>
+</header>
+)
+}
+
+/**
+ * Passage de props pour les titres et les enfants JSX des différentes Sections
+ * @returns ContainerJSX
+ */
+function Container({title,children}:any) {
+  return (
+  <div className=" border-solid border-2 border-blue-800 bg-amber-700">
+    <div>
+     <h1>{title}</h1>
+    </div>
+   {children}
+   </div>
+)
+}
+
+  /**
+   * Enfant JSX ( Avatar, Text, Réseaux, etc...)
+   * @returns Enfant JSX
+   * Text : Retourne une chaine de caractère
+   * Links : Retourne 2 Liens
+   */
+
+  function Text({text}:any) {
+
+    return (
+      <div>
+                  <p>{text}</p>
+
+      </div>
+    )
+  }
+
+  function Links({Llinkedin,Lgit,gitHub, LinkeDin}:any) {
+    return (
+      <div>
+        <ul>
+          <li><a href={Llinkedin}>{gitHub}</a></li>
+          <li><a href={Lgit}>{LinkeDin}</a></li>
+        </ul>
+      </div>
+    )
+  }
+
+  function Icons({icons,text}:any){
+      return (
+        <div>
+                  <ul>
+          <li>{icons}<p>{text}</p></li>
+        </ul>
+
         </div>
-      </div>
+      )
+  }
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  function Avatar({img}:any) {
+    return (
+      <div>
+              <h1>{img}</h1>
+
+      </div>
+    ) 
+  }
+
+function SkillTitle({title}:any) {
+  return <div> <h2>{title}</h2> </div>
+}
+
+function ContainerWorks({children}:any) {
+  return (
+  <div className="bg-gray-400">{children}</div>
+)
+}
+
+
+
+/**
+ * 
+ * @returns Footer JSX
+ */
+function Footer({icons}:any) {
+  return (
+    <footer>
+    <div className="bg-gray-400">
+<a href="">{icons}</a>  
+  </div>
+    </footer>
+)
+}
+
+/**
+ * Function all in one
+ * Call all JSX Element
+ * @returns all jsx element
+ */
+export default function Site() {
+
+return (
+  <main className="w-full h-full">
+
+      <Nav/>
+
+    <div>
+      <Container id="About" title="A Propos">
+        <Avatar img="mettre une image ici et remplacer le h1 par un img:s"/>
+          <Text text="lorem ipsum"/>
+          <Links gitHub="Lien GitHub"
+                LinkeDin="Lien LinkeDin"   
+                Llinkedin = "https://www.youtube.com/results?search_query=jsx+tuto+"
+                Lgit = "https://www.youtube.com/results?search_query=jsx+tuto+"    
         />
-      </div>
+        <Icons icons="icons"
+               text="text" 
+        />
+        <Icons icons="icons"
+               text="text" 
+        />
+        <Icons icons="icons"
+               text="text" 
+        />
+        <Icons icons="icons"
+               text="text" 
+        />
+        
+      </Container>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <Container id="skills" title="Compétence">
+          <SkillTitle title="Language"/>
+          <Icons icons="icons"
+               text="text" 
+        />
+            <Icons icons="icons"
+               text="text" 
+        />
+            <Icons icons="icons"
+               text="text" 
+        />
+            <Icons icons="icons"
+               text="text" 
+        />
+            <Icons icons="icons"
+               text="text" 
+        />
+            <Icons icons="icons"
+               text="text" 
+        />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <SkillTitle title="Framework"/>
+          <Icons icons="icons"
+               text="text" 
+        />
+            <Icons icons="icons"
+               text="text" 
+        />
+            <Icons icons="icons"
+               text="text" 
+        />
+            <Icons icons="icons"
+               text="text" 
+        />
+            <Icons icons="icons"
+               text="text" 
+        />
+            <Icons icons="icons"
+               text="text" 
+        />
+          <SkillTitle title="others"/>
+          <Icons icons="icons"
+               text="text" 
+        />
+            <Icons icons="icons"
+               text="text" 
+        />
+            <Icons icons="icons"
+               text="text" 
+        />
+            <Icons icons="icons"
+               text="text" 
+        />
+            <Icons icons="icons"
+               text="text" 
+        />
+            <Icons icons="icons"
+               text="text" 
+        />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </Container>
+        
+      <Container  title="Parcours">
+        <ContainerWorks className="bg-gray-400">
+          <SkillTitle title="un titre"/>
+          <Text text="un text"/>
+        </ContainerWorks>
+
+        <ContainerWorks className="bg-gray-400">
+          <SkillTitle title="un titre"/>
+          <Text text="un text"/>
+        </ContainerWorks>
+
+
+        <ContainerWorks className="bg-gray-400">
+          <SkillTitle title="un titre"/>
+          <Text text="un text"/>
+        </ContainerWorks>
+
+      </Container>
+
+      <Container  title="Portofolio"/>
+    </div>
+   
+
+  <Container>
+     <Footer icons="telephone"/>
+     <Footer icons="mail"/>
+     <Footer icons="dl"/>
+
+  </Container>
+   
+    
+  </main>
   );
 }
