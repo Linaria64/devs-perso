453035f73e626c03292cff5f32f6c295f5f1c35c
Linaria64 <dorianpro77@proton.me>
2024-07-24 19:04:37 +0200

    #6 Archi Contact Page

--- lindev/src/pages/contact.js
+++ lindev/src/pages/contact.js
@@ -1,12 +1,38 @@
 import React from "react";
 
 function Contact() {
-    <>
-    return(
-        <h1>Titre de la page de contact</h1>
-    );
-    </>
 
+    return (
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <p>Strudergnach</p>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <div>
+                        <p>Leo</p>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <div>
+                        <p>Imanemail@mail.xx</p>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <button>Neon</button>
+            </div>
+        </div>
+    );
 }
 
 export default Contact;
