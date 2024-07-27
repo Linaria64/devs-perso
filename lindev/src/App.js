a3f4b016e8d72df1e3bf76356c763ba204f27140
Linaria64 <dorianpro77@proton.me>
2024-07-27 13:24:28 +0200

    #7 Navbar Design

--- lindev/src/App.js
+++ lindev/src/App.js
@@ -5,17 +5,24 @@ import Contact from './pages/contact';
 import Works from './pages/works';
 import Pricing from './pages/pricing';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import logo from './img/Logo_lin-dev.png';
 function App() {
   return (
      <Router>
-      <div className="h-screen w-full bg-cover bg-center text-white" style={{ backgroundImage: "url('/fond_noir_sobre_ia.png')"}}>
-        <nav>
-          <ul className='flex gap-8'>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/works">Works</Link></li>
-            <li><Link to="/pricing">Pricing</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+      <div className="min-h-screen min-w-full  flex flex-col bg-cover bg-center text-white" style={{ backgroundImage: "url('/fond_noir_sobre_ia.png')"}}>
+        <nav className=' flex gap-x-8  mx-auto p-5'>
+          <div>
+            <img src={logo} alt='Logo Lin_Dev avec un effet néon' className='rounded-lg w-28'/>
+          </div>
+        <div className=' bg-[#1B1B1E] bg-opacity-50 rounded-lg '>
+            <ul className='flex gap-4 justify-center p-2 '>
+            <li className='bg-[#333] p-3 rounded-lg'><Link to="/about">About</Link></li>
+            <li className='bg-[#333] p-3 rounded-lg'><Link to="/about">Works</Link></li>
+            <li className='bg-[#333] p-3 rounded-lg'><Link to="/works">Home</Link></li>
+            <li className='bg-[#333] p-3 rounded-lg'><Link to="/pricing">Pricing</Link></li>
+            <li className='bg-[#333] p-3 rounded-lg'><Link to="/contact">Contact</Link></li>
           </ul>
+        </div>
         </nav>
 
         <Routes>
--- lindev/src/tailwind.config.js
+++ lindev/src/tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     theme: {
       extend: {
+        colors: {
+          b_antra: '#333',
+        },
         backgroundImage: {
           'fondnoir': "url('../public/fond_noir_sobre_ia.png')",
         }
